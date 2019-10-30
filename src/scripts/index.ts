b31a24566a70ff05264b3e96b46f8c639c61819b
Phoomparin Mano <phoomparin@gmail.com>
2019-10-30 04:15:48 -0700

    :sparkles: Improve data binding script

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -3,39 +3,41 @@ interface NavbarItem {
   label: string
 }
 
-interface ContentResponse {
+interface PageData {
   condition: string
   detail: string
   duration: string
   navbarItems: NavbarItem[]
 }
 
-async function loadContent(): Promise<ContentResponse> {
+async function loadPageData(): Promise<PageData> {
   const payload = await fetch('https://panjs.com/ywc.json')
 
   return payload.json()
 }
 
 function setNavbar(items: NavbarItem[]) {
-  const navEl = document.querySelector('header.navbar')
+  const element = document.querySelector('header.navbar')
+  if (!element) return
 
-  for (let item of items) {
-    navEl.innerHTML += `<a href="${item.href}" class="nav-link fade in" target="_blank">${item.label}</a>`
-  }
+  items.forEach(item => {
+    element.innerHTML += `<a href="${item.href}" class="nav-link fade in" target="_blank">${item.label}</a>`
+  })
 }
 
-function setDuration(duration: string) {
-  const durationEl = document.querySelector('.duration')
-
-  durationEl.textContent = duration
+function setData(selector: string, data: string) {
+  const element = document.querySelector(selector)
+  if (element) element.innerHTML = data
 }
 
 async function onReady() {
-  const content = await loadContent()
-  console.log(content)
+  const data = await loadPageData()
 
-  setNavbar(content.navbarItems)
-  setDuration(content.duration)
+  setNavbar(data.navbarItems)
+
+  setData('.duration', data.duration)
+  setData('.detail', data.detail)
+  setData('.condition', data.condition)
 }
 
 document.addEventListener('DOMContentLoaded', onReady)
